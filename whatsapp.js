755d670577e980ae73bae550c41cacaa630a1838
agunbuhori <agun@buhori.com>
2019-11-28 16:46:45 +0700

    WhatsApp updated

--- whatsapp.js
+++ whatsapp.js
@@ -1,4 +1,3 @@
-const app = require('express')();
 const sulla = require('sulla');
 const request = require('request');
 const webhook = "https://api-register.tsl-university.id/receive_message/5dddd9bec1bfdf3a09adda7d";
@@ -15,22 +14,12 @@ function start(client) {
     request.post({url: webhook, body: JSON.stringify(requestMessage), headers: {"Content-Type": "application/json"}}, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the response status code if a response was received
 
         let snapshot = JSON.parse(body);
 
-        if (response && response.statusCode === 200 && response.reply)
+        if (response.statusCode === 200 && snapshot && snapshot.reply)
             client.sendText(requestMessage.number, snapshot.message);
     });
   });
-
-  app.get("/", function (req, res) {
-    res.send("WhatsApp server is working fine");
-  });
-
-  app.post("/send", function (req, res) {
-    client.sendText(req.query.to+"@c.us", req.query.body);
-    res.send("Message has been sent");
-  });
 }
-
-app.listen(2113);
