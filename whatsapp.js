c255c334bb5f90f486b66b7a053d6217d8762e8c
agunbuhori <agun@buhori.com>
2019-11-29 10:05:15 +0700

    Package updated to sulla-hotfix

--- whatsapp.js
+++ whatsapp.js
@@ -1,11 +1,12 @@
-const sulla = require('sulla');
+const sulla = require('sulla-hotfix');
 const request = require('request');
 const webhook = "https://api-register.tsl-university.id/receive_message/5dddd9bec1bfdf3a09adda7d";
 
 sulla.create().then(client => start(client));
 
-
 function triggerServer(requestMessage, client) {
+  if (requestMessage.message.match(/TSL/))
+  
   request.post({url: webhook, body: JSON.stringify(requestMessage), headers: {"Content-Type": "application/json"}}, function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -25,6 +26,28 @@ function start(client) {
         message: message.body
     };
 
-    triggerServer(requestMessage, client);    
+    if (typeof requestMessage.message === 'string')
+      triggerServer(requestMessage, client);    
   });
+
+  setTimeout(() => {
+    client.getUnreadMessages(true, true, true).then(chats => {
+      chats.forEach(function (chat, index) {
+          if (! chat.isGroup) {
+              chat.messages.forEach(function (message, index) {
+
+                  if (typeof message.body === 'string') {
+                    const requestMessage = {
+                        number: message.from._serialized,
+                        message: message.body
+                    };
+
+                    if (index === chat.messages.length-1)
+                      triggerServer(requestMessage, client);
+                  }
+              });
+          }
+      });
+    });
+  }, 6000);
 }
